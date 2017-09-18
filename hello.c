5ea397233cb5fa4b7173cf1f1b43931130b3217a
yejordan <yejordan@gmail.com>
2017-09-18 20:06:54 +0800

    initial program with two input

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 int main()
 
  {      int number;
+	float num2;
 	printf("please input a number:");
-	scanf("%d",&number);
+	scanf("%c",&number);
+	scanf("%f",&num2);
 	printf("You enter this number:%d \n",number);
+	printf("You enter num2:%f \n",num2);
 	return 0;
  }
